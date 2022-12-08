508fb48c126bf0460dccf7a311017b26deeacc77
Nabeel Akbar <studiosnabeel@gmail.com>
2022-12-08 19:14:19 +0500

    onClick event added to Calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,41 +1,186 @@
+/* eslint-disable react/no-unused-state */
+/* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
+import React from 'react';
 
-class Calculator extends Component {
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { result: 0, operator: '', operand: 0 };
+  }
+
+  handleChange = (e) => {
+    const op = e.target.innerHTML;
+    this.setState({ operand: op });
+  };
+
+  handleOperator = (e) => {
+    this.setState({ operator: e.target.innerHTML });
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ result: 0 });
+  };
+
   render() {
     return (
       <div className="Calculator">
+        <form className="row">
+          <input
+            disabled
+            className="res"
+            value={this.state.operand}
+            onChange={(e) => {
+              this.setState({ operand: e.target.value });
+            }}
+          />
+        </form>
         <div className="row">
-          <div className="res">0</div>
+          <button
+            type="button"
+            id="AC"
+            onClick={() => this.setState({ result: 0 })}
+            className="pads"
+          >
+            AC
+          </button>
+          <button
+            type="button"
+            onClick={(e) => this.handleOperator(e)}
+            id="+/-"
+            className="pads"
+          >
+            +/-
+          </button>
+          <button
+            type="button"
+            onClick={(e) => this.handleOperator(e)}
+            id="%"
+            className="pads"
+          >
+            %
+          </button>
+          <button
+            type="button"
+            onClick={(e) => this.handleOperator(e)}
+            className="pads"
+          >
+            &#247;
+          </button>
         </div>
         <div className="row">
-          <div className="pads">AC</div>
-          <div className="pads">+/-</div>
-          <div className="pads">%</div>
-          <div className="pads">&#247;</div>
+          <button
+            className="pads"
+            type="button"
+            onClick={(e) => this.handleChange(e)}
+          >
+            7
+          </button>
+          <button
+            className="pads"
+            type="button"
+            onClick={(e) => this.handleChange(e)}
+          >
+            8
+          </button>
+          <button
+            className="pads"
+            type="button"
+            onClick={(e) => this.handleChange(e)}
+          >
+            9
+          </button>
+          <button
+            className="pads"
+            type="button"
+            onClick={(e) => this.handleOperator(e)}
+          >
+            x
+          </button>
         </div>
         <div className="row">
-          <div className="pads">7</div>
-          <div className="pads">8</div>
-          <div className="pads">9</div>
-          <div className="pads">x</div>
+          <button
+            className="pads"
+            type="button"
+            onClick={(e) => this.handleChange(e)}
+          >
+            4
+          </button>
+          <button
+            className="pads"
+            type="button"
+            onClick={(e) => this.handleChange(e)}
+          >
+            5
+          </button>
+          <button
+            className="pads"
+            type="button"
+            onClick={(e) => this.handleChange(e)}
+          >
+            6
+          </button>
+          <button
+            className="pads"
+            type="button"
+            onClick={(e) => this.handleOperator(e)}
+          >
+            -
+          </button>
         </div>
         <div className="row">
-          <div className="pads">4</div>
-          <div className="pads">5</div>
-          <div className="pads">6</div>
-          <div className="pads">-</div>
+          <button
+            className="pads"
+            type="button"
+            onClick={(e) => this.handleChange(e)}
+          >
+            1
+          </button>
+          <button
+            className="pads"
+            type="button"
+            onClick={(e) => this.handleChange(e)}
+          >
+            2
+          </button>
+          <button
+            className="pads"
+            type="button"
+            onClick={(e) => this.handleChange(e)}
+          >
+            3
+          </button>
+          <button
+            className="pads"
+            type="button"
+            onClick={(e) => this.handleOperator(e)}
+          >
+            +
+          </button>
         </div>
         <div className="row">
-          <div className="pads">1</div>
-          <div className="pads">2</div>
-          <div className="pads">3</div>
-          <div className="pads">+</div>
-        </div>
-        <div className="row">
-          <div className="pads zero">0</div>
-          <div className="pads">.</div>
-          <div className="pads">=</div>
+          <button
+            className="pads zero"
+            type="button"
+            onClick={(e) => this.handleChange(e)}
+          >
+            0
+          </button>
+          <button
+            className="pads"
+            type="button"
+            onClick={(e) => this.handleChange(e)}
+          >
+            .
+          </button>
+          <button
+            className="pads"
+            type="button"
+            onClick={(e) => this.handleOperator(e)}
+          >
+            =
+          </button>
         </div>
       </div>
     );
