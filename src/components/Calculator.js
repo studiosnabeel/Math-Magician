c8bf19e48fb66f2a995a5d4f342486403d5a69c3
Nabeel Akbar <studiosnabeel@gmail.com>
2022-12-08 22:28:54 +0500

    modify code in calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,25 +2,21 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import Calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { result: 0, operator: '', operand: 0 };
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
   }
 
   handleChange = (e) => {
-    const op = e.target.innerHTML;
-    this.setState({ operand: op });
-  };
-
-  handleOperator = (e) => {
-    this.setState({ operator: e.target.innerHTML });
-    this.reset();
-  };
-
-  reset = () => {
-    this.setState({ result: 0 });
+    const res = Calculate(this.state, e.target.innerHTML);
+    this.setState(res);
   };
 
   render() {
@@ -30,24 +26,21 @@ class Calculator extends React.Component {
           <input
             disabled
             className="res"
-            value={this.state.operand}
-            onChange={(e) => {
-              this.setState({ operand: e.target.value });
-            }}
+            value={this.state.next || this.state.total || 0}
           />
         </form>
         <div className="row">
           <button
             type="button"
             id="AC"
-            onClick={() => this.setState({ result: 0 })}
+            onClick={this.handleChange}
             className="pads"
           >
             AC
           </button>
           <button
             type="button"
-            onClick={(e) => this.handleOperator(e)}
+            onClick={this.handleChange}
             id="+/-"
             className="pads"
           >
@@ -55,107 +48,55 @@ class Calculator extends React.Component {
           </button>
           <button
             type="button"
-            onClick={(e) => this.handleOperator(e)}
+            onClick={this.handleChange}
             id="%"
             className="pads"
           >
             %
           </button>
-          <button
-            type="button"
-            onClick={(e) => this.handleOperator(e)}
-            className="pads"
-          >
+          <button type="button" onClick={this.handleChange} className="pads">
             &#247;
           </button>
         </div>
         <div className="row">
-          <button
-            className="pads"
-            type="button"
-            onClick={(e) => this.handleChange(e)}
-          >
+          <button className="pads" type="button" onClick={this.handleChange}>
             7
           </button>
-          <button
-            className="pads"
-            type="button"
-            onClick={(e) => this.handleChange(e)}
-          >
+          <button className="pads" type="button" onClick={this.handleChange}>
             8
           </button>
-          <button
-            className="pads"
-            type="button"
-            onClick={(e) => this.handleChange(e)}
-          >
+          <button className="pads" type="button" onClick={this.handleChange}>
             9
           </button>
-          <button
-            className="pads"
-            type="button"
-            onClick={(e) => this.handleOperator(e)}
-          >
+          <button className="pads" type="button" onClick={this.handleChange}>
             x
           </button>
         </div>
         <div className="row">
-          <button
-            className="pads"
-            type="button"
-            onClick={(e) => this.handleChange(e)}
-          >
+          <button className="pads" type="button" onClick={this.handleChange}>
             4
           </button>
-          <button
-            className="pads"
-            type="button"
-            onClick={(e) => this.handleChange(e)}
-          >
+          <button className="pads" type="button" onClick={this.handleChange}>
             5
           </button>
-          <button
-            className="pads"
-            type="button"
-            onClick={(e) => this.handleChange(e)}
-          >
+          <button className="pads" type="button" onClick={this.handleChange}>
             6
           </button>
-          <button
-            className="pads"
-            type="button"
-            onClick={(e) => this.handleOperator(e)}
-          >
+          <button className="pads" type="button" onClick={this.handleChange}>
             -
           </button>
         </div>
         <div className="row">
-          <button
-            className="pads"
-            type="button"
-            onClick={(e) => this.handleChange(e)}
-          >
+          <button className="pads" type="button" onClick={this.handleChange}>
             1
           </button>
-          <button
-            className="pads"
-            type="button"
-            onClick={(e) => this.handleChange(e)}
-          >
+          <button className="pads" type="button" onClick={this.handleChange}>
             2
           </button>
-          <button
-            className="pads"
-            type="button"
-            onClick={(e) => this.handleChange(e)}
-          >
+          <button className="pads" type="button" onClick={this.handleChange}>
             3
           </button>
-          <button
-            className="pads"
-            type="button"
-            onClick={(e) => this.handleOperator(e)}
-          >
+          <button className="pads" type="button" onClick={this.handleChange}>
             +
           </button>
         </div>
@@ -163,22 +104,14 @@ class Calculator extends React.Component {
           <button
             className="pads zero"
             type="button"
-            onClick={(e) => this.handleChange(e)}
+            onClick={this.handleChange}
           >
             0
           </button>
-          <button
-            className="pads"
-            type="button"
-            onClick={(e) => this.handleChange(e)}
-          >
+          <button className="pads" type="button" onClick={this.handleChange}>
             .
           </button>
-          <button
-            className="pads"
-            type="button"
-            onClick={(e) => this.handleOperator(e)}
-          >
+          <button className="pads" type="button" onClick={this.handleChange}>
             =
           </button>
         </div>
