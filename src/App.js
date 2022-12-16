cdaec64a02619977c9ec85b7fb8b53cfd2124cf0
Nabeel Akbar <studiosnabeel@gmail.com>
2022-12-16 18:34:57 +0500

    Modify app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Quote from './pages/Quote';
 import Math from './pages/Math';
+import Header from './components/Header';
 
 const App = () => {
   <>
+    <Header />
     <div className="folder">
       <Routes>
         <Route exact path="/" element={<Home />} />
