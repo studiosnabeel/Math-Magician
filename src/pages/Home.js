132a4b34cd43364ba77428c5aa5d6d3e5f1e5354
Nabeel Akbar <studiosnabeel@gmail.com>
2022-12-16 20:38:46 +0500

    Add  more style with css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,8 @@
 /* eslint-disable max-len */
 import React from 'react';
 
-const Home = () => {
-  <div>
+const Home = () => (
+  <div className="Home">
     <h2>Welcome!</h2>
     <p>
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi nam cumque
@@ -10,13 +10,14 @@ const Home = () => {
       vel omnis quis fugiat provident quibusdam nobis rem dolor assumenda aut?
       Sed ipsa nam molestiae molestias saepe inventore! Velit!
     </p>
+
     <p>
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi nam cumque
       similique quidem nemo alias iusto officia, unde doloribus iste suscipit
       vel omnis quis fugiat provident quibusdam nobis rem dolor assumenda aut?
       Sed ipsa nam molestiae molestias saepe inventore! Velit!
     </p>
-  </div>;
-};
+  </div>
+);
 
 export default Home;
