2397d187a4b507c86a24de18bb6cf19c41db11c2
Nabeel Akbar <studiosnabeel@gmail.com>
2022-12-16 14:50:49 +0500

    Add Routes and Route for pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-import Calculator from './components/Calculator';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
+import Math from './pages/Math';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="folder">
-        <Calculator />
-      </div>
-    );
-  }
-}
+const App = () => {
+  <>
+    <div className="folder">
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/calculator" element={<Math />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+    </div>
+  </>;
+};
 
 export default App;
---
+++ src/pages/Quote.js
@@ -0,0 +1,13 @@
+/* eslint-disable max-len */
+import React from 'react';
+
+const Quote = () => {
+  <div>
+    <h2 className="Quote">
+      Mathematics is not about numbers, equations, computations, or algorithms:
+      it is about understanding. -William Paul Thurston
+    </h2>
+  </div>;
+};
+
+export default Quote;
